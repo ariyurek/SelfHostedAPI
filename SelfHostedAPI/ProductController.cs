aa3bba51f70fd844069d6961ee932319dd2c757e
ariyurek <ariyurek@yahoo.com>
2019-03-24 12:13:07 +0100

    Add project files.

---
+++ SelfHostedAPI/ProductController.cs
@@ -0,0 +1,75 @@
+﻿#region Using namespaces
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http; 
+#endregion
+
+namespace SelfHostedAPI
+{
+    /// <summary>
+    /// Product controller. Contains CURD operations on product
+    /// </summary>
+    public class ProductController : ApiController
+    {
+        private WebApiDbContext db = new WebApiDbContext();
+        // GET api/<controller>
+        public IEnumerable<Product> Get()
+        {
+            return db.Product;
+           //var entities=new WebApiDbEntities();
+           // return entities.Products;
+        }
+
+        // GET api/<controller>/5
+        public Product Get(int id)
+        {
+            return db.Product.FirstOrDefault(p => p.ProductId == id);
+            //var entities = new WebApiDbEntities();
+            //return entities.Products.FirstOrDefault(p=>p.ProductId==id);
+        }
+
+        // POST api/<controller>
+        public bool Post([FromBody]Product product)
+        {
+            //var entities = new WebApiDbEntities();
+            //entities.AddToProducts(product);
+            //var i = entities.SaveChanges();
+            db.Product.Add(product);
+            var i =  db.SaveChanges();
+
+            return i > 0;
+        }
+
+        // PUT api/<controller>/5
+        public bool Put(int id, [FromBody]Product product)
+        {
+            return true;
+            //using (var entities = new WebApiDbEntities())
+            //{
+            //    var prod = (from p in entities.Products
+            //                where p.ProductId == id
+            //                select p).FirstOrDefault();
+
+            //    prod.ProductName = product.ProductName;
+            //    var i=entities.SaveChanges();
+            //    return i > 0;
+            //}
+        }
+
+        // DELETE api/<controller>/5
+        public bool Delete(int id)
+        {
+            return true;
+            //using (var entities = new WebApiDbEntities())
+            //{
+            //    var prod = (from p in entities.Products
+            //                where p.ProductId == id
+            //                select p).FirstOrDefault();
+
+            //   entities.Products.DeleteObject(prod);
+            //   var i=entities.SaveChanges();
+            //   return i > 0;
+            //}
+        }
+    }
+}
---
+++ SelfHostedAPI/WebApiDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace SelfHostedAPI
+{
+    public class WebApiDbContext : DbContext
+    {
+        public WebApiDbContext() : base("name=WebApiDbEntities")
+        {
+             
+        }
+
+        public System.Data.Entity.DbSet<SelfHostedAPI.Product> Product { get; set; }
+         
+    }
+}
